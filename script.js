137f35bf2e3a8dad50b0571629e7c9421d69d9a2
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-30 18:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+document.getElementById("ageForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // prevent page reload
+
+  const name = document.getElementById("name").value.trim();
+  const ageValue = document.getElementById("age").value;
+  const age = parseInt(ageValue);
+
+  if (name === "" || ageValue === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  checkAge(name, age)
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+});
+function checkAge(name,age){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age >=18){
+				resolve(`Welcome, ${name}. You can vote.`)
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		})
+	})
+}
